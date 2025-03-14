1391cb0006fefbcc93797be7c2db9c7530bcb132
Harri <70561842+harri-k-hanninen@users.noreply.github.com>
2025-03-14 15:07:42 +0200

    Update venue_format_converter.py
    update from Comma to Semicolon separation.

--- venue_format_converter.py
+++ venue_format_converter.py
@@ -7,18 +7,23 @@ searchString = "KJP"
 
 # Take the vakiovuorot PDF file that City of Kouvola provides, ctrl+a and then ctrl+c to copy to clipboard.
 # Then paste as PLAIN TEXT and save to 'vakiovuorot_dump.txt', which will then be parsed by this script.
-
-file = open('vakiovuorot_dump.txt')
-lines = file.readlines()
-file.close()
+try:
+    with open('vakiovuorot_dump.txt') as file:
+        lines = file.readlines()
+except FileNotFoundError:
+    print("Error: vakiovuorot_dump.txt file not found")
+    sys.exit(1)
+except IOError:
+    print("Error: Could not read vakiovuorot_dump.txt")
+    sys.exit(1)
 
 # weekdays as in MA/TI/KE/TO/PE/LA/SU (Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday), initialize to None and change during for loop.
 weekday = None
 # Site as in "Kohde" in the original, initialize to None and change during for loop.
 site = None
 
-# Print the header of the CSV file.
-print("Tila,viikonpäivä,Tilatarkennus,Klo,Ryhmä,Aikaväli")
+# Print the header of the CSV file, where SEMICOLON is separator.
+print("Tila;viikonpäivä;Tilatarkennus;Klo;Ryhmä;Aikaväli")
 
 # Go through the dump line by line...
 for i in range(len(lines)):
@@ -32,13 +37,13 @@ for i in range(len(lines)):
     # continue to parse the page by looking at weekdays    
     elif "MA\n" in lines[i] or "TI\n" in lines[i] or "KE\n" in lines[i] or "TO\n" in lines[i] or "PE\n" in lines[i] or "LA\n" in lines[i] or "SU\n" in lines[i]:
         weekday = str(re.sub('\n', '', lines[i]))
-        
+
     # and finally see if there are matches to your search-string
     elif searchString in lines[i]:
         # check if there is "tilatarkennus" or no... because the formatting is different if there is.
         if ".2024" in lines[i-2] or ".2025" in lines[i-2] or ".2026" in lines[i-2] or "MA\n" in lines[i-2] or "TI\n" in lines[i-2] or "KE\n" in lines[i-2] or "TO\n" in lines[i-2] or "PE\n" in lines[i-2] or "LA\n" in lines[i-2] or "SU\n" in lines[i-2]:
-            print(site + "," + weekday + ",ei-tilatarkennusta," + re.sub('\n', '', lines[i-1]) + "," + re.sub('\n', '', lines[i]) + "," + re.sub('\n', '', lines[i+1]))
+            print(site + ";" + weekday + ";ei-tilatarkennusta;" + re.sub('\n', '', lines[i-1]) + ";" + re.sub('\n', '', lines[i]) + ";" + re.sub('\n', '', lines[i+1]))
         else:
-            print(site + "," + weekday + "," + re.sub('\n', '', lines[i-2]) + "," + re.sub('\n', '', lines[i-1]) + "," + re.sub('\n', '', lines[i]) + "," + re.sub('\n', '', lines[i+1]))
+            print(site + ";" + weekday + ";" + re.sub('\n', '', lines[i-2]) + ";" + re.sub('\n', '', lines[i-1]) + ";" + re.sub('\n', '', lines[i]) + ";" + re.sub('\n', '', lines[i+1]))
 
-# At the end, you should have COMMA separated format output in your terminal/console, that you can then copy paste to spreadsheet tool you like, i.e. Microsoft Excel.
+# At the end, you should have SEMICOLON separated format output in your terminal/console, that you can then copy paste to spreadsheet tool you like, i.e. Microsoft Excel.
